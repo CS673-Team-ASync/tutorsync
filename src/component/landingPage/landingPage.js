09139c2c94ad1573463df0abe3ca241589ec3a3a
Schang0 <shannen@bu.edu>
2021-08-12 22:28:39 +0800

    updated landing page

--- src/component/landingPage/landingPage.js
+++ src/component/landingPage/landingPage.js
@@ -34,12 +34,15 @@ const LandingPage = ({ isLoggedIn, setIsLoggedIn }) => {
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
                         <Form.Control type="email" placeholder="Email Address" />
-                        
+                        <input type="text" id="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
                         <Form.Control type="password" placeholder="Password" />
+                        <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+                        
                     </Form.Group>
 
                     <Row className="justify-content-md-center">
